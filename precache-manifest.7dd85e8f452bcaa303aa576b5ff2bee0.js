6f9f71ab5ab77c35f179d3147af77eb5d477ed12
POWER USER <poweruser@Users-iMac-2.local>
2019-01-01 22:36:17 -0500

    Updates

---
+++ precache-manifest.7dd85e8f452bcaa303aa576b5ff2bee0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4944ede344eb2f532d3b",
+    "url": "/reactreduxauth/static/css/main.8276f604.chunk.css"
+  },
+  {
+    "revision": "4944ede344eb2f532d3b",
+    "url": "/reactreduxauth/static/js/main.4944ede3.chunk.js"
+  },
+  {
+    "revision": "663515b4f39f1823b529",
+    "url": "/reactreduxauth/static/js/1.663515b4.chunk.js"
+  },
+  {
+    "revision": "6bfdbf41e1417fa7b989",
+    "url": "/reactreduxauth/static/js/runtime~main.6bfdbf41.js"
+  },
+  {
+    "revision": "54ecdf23d4481ec31a0700e7449f88af",
+    "url": "/reactreduxauth/index.html"
+  }
+];
